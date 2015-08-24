b5277cc411aa1c7c48d2d99a7aaa13d23d2766b9
Maxim Veksler <maxim@vekslers.org>
2015-08-24 21:39:50 +0300

    Calculations.

--- lesson-002/Calculator/Calculator/ViewController.swift
+++ lesson-002/Calculator/Calculator/ViewController.swift
@@ -22,5 +22,61 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+        case "×":
+            performOperation { $0 * $1 } // bonus: explain why ```performOperation(*)``` also works?
+        case "÷":
+            performOperation { $1 / $0 }
+        case "+":
+            performOperation { $0 + $1 }
+        case "−":
+            performOperation { $1 - $0 }
+        case "√":
+            performOperation { sqrt($0) }
+        default:
+            break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    @nonobjc // Swift 1.2 incompatability workaround. See http://stackoverflow.com/a/31500740/48062
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
