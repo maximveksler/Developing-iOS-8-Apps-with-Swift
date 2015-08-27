e7f7adc9635c1de04f91d77dee8e3d96cc4ea973
Maxim Veksler <maxim@vekslers.org>
2015-08-28 00:51:03 +0300

    UIPanGestureRecognizer

--- lesson-006/Happiness/Happiness/HappinessViewController.swift
+++ lesson-006/Happiness/Happiness/HappinessViewController.swift
@@ -17,6 +17,26 @@ class HappinessViewController: UIViewController, FaceViewDatasource {
         }
     }
     
+    private struct Constants {
+        static let HappinessGeastureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(geasture: UIPanGestureRecognizer) {
+        switch geasture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = geasture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGeastureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                geasture.setTranslation(CGPointZero, inView: faceView)
+            }
+            
+        default: break
+            
+        }
+    }
+    
     var happiness: Int = 75 { // 0 = very sad 100 = ecstatic
         didSet {
             happiness = min(max(happiness, 0), 100)
