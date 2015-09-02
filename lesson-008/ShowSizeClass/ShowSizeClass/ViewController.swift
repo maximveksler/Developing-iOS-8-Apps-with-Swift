4db7b6871b06475f653ea89505b849d438d87ef1
Maxim Veksler <maxim@vekslers.org>
2015-09-02 12:49:38 +0300

    AnyAny :)

---
+++ lesson-008/ShowSizeClass/ShowSizeClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowSizeClass
+//
+//  Created by Maxim Veksler on 31/08/2015.
+//  Copyright © 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
